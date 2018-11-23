ef4c188b21cb2c612f64daf5712d06659510c1ac
jiaxiaole <jiaxiaole_2009@163.com>
2018-11-23 20:48:46 +0800

    开始wx：for改造程序

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,12 +2,53 @@
 //获取应用实例
 
 Page({
- data:{
-   titles: ['国内', '国际', '财经', '娱乐', '军事', '体育', '其他']
- },
+  data: {
+    titles: ['国内', '国际', '财经', '娱乐', '军事', '体育', '其他'],
+    newsList: [],
+    newsHot:{},
+  },
   onTapDetail() {
     wx.navigateTo({
       url: '/pages/detail/detail',
     })
   },
+  getNewsList(type) {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/list', //仅为示例，并非真实的接口地址
+      data: {
+        type: type,
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: res => {
+        console.log(res.data)
+        console.log(res.data.result)
+        let result = res.data.result
+        this.setHot(result)
+        this.setNewsList(result)
+
+
+      },
+      // complete: () => {
+      //   callback && callback()
+      // }
+    })
+  },
+  setHot(result){
+    let hot = result[0]
+    
+    console.log(hot)
+    this.setData({
+      newsHot:hot
+    })
+  },
+  setNewsList() {
+
+  },
+  onLoad() {
+    this.getNewsList("gn")
+  },
+  
+
 })
